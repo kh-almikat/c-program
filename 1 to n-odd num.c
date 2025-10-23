47d26b3a1656c4e29d27745b8321fc786bac624d
kh-almikat <khondoker.almikat@gmail.com>
2025-10-23 12:24:51 +0600

    Update 1 to n-odd num.c

--- 1 to n-odd num.c
+++ 1 to n-odd num.c
@@ -2,28 +2,18 @@
 #include<conio.h>
 int main()
 {
-    // int n,i,sum=0;
-    // printf("enter last number:");
-    // scanf("%d",&n);
+     int n,i,sum=0;
+     printf("enter last number:");
+     scanf("%d",&n);
 
-    // for(i=1;i<=n;i=i+2)
-    // {
-    //     sum=sum+i;
-    //     printf("%d,",i);
-    // }
-    // printf("\n total odd num sum:%d ",sum);
-
-    // getch();
-    int a;
-     printf("enter last number: ");
-     scanf("%d",&a);
-
-     if(a % 2 != 0)
+     for(i=1;i<=n;i=i+2)
      {
-        printf("Number is odd.");
+         sum=sum+i;
+         printf("%d,",i);
      }
-     else
-     {
-        printf("Number is even");
-     }
+     printf("\n total odd num sum:%d ",sum);
+
+     getch();
+
 }
+
